4abfbb9de2e01452de036e9c927f63c43cc3df03
egd0r <edgarv1@pm.me>
2025-02-16 18:53:23 +0000

    FEAT-9: [REPO] Add UnityTest Support
    Added Unity support for unit tests
    
    SCS... oh.. wait...

---
+++ tests/libs/test_communication.c
@@ -0,0 +1,25 @@
+#include "unity.h"
+/* Include libs here */
+
+void setUp(void) {
+    /* Set up test here */
+}
+
+void tearDown(void) {
+    /* Clean up test here */
+}
+
+void test_test(void) {
+    /**
+     * TEST_ASSERT_EQUAL( *expected value* , *function return* )
+     * look at unity docs
+     */
+    TEST_ASSERT_EQUAL(0, 0);
+}
+
+
+int main(void) {
+    UNITY_BEGIN();
+    RUN_TEST(test_test);
+    return UNITY_END();
+}
